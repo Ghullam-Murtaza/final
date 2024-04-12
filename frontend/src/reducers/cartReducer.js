011511fe185515bad43b16c0b34f0781de54bcc4
Ghullam-Murtaza <130633368+Ghullam-Murtaza@users.noreply.github.com>
2024-04-12 09:10:34 -0700

    Update cartReducer.js

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -1,4 +1,5 @@
 
+
 import { ADD_TO_CART, EMPTY_CART, REMOVE_FROM_CART, SAVE_SHIPPING_INFO } from "../constants/cartConstants";
 
 export const cartReducer = (state = { cartItems: [], shippingInfo: {} }, { type, payload }) => {
